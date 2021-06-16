6a7a98ed16959938c9b0dde631bfc2d5eb086663
Abdelaziz Bel haj <abel-haj@e1r2p8.1337.ma>
2021-06-16 21:59:34 +0100

    printing/looping/catching problem

--- reciever.c
+++ reciever.c
@@ -3,11 +3,24 @@
 int	i;
 int	got[8];
 
+int	power(int base, int power)
+{
+	int	result;
+
+	result = 1;
+	while (power > 0)
+	{
+		result = result * base;
+	}
+	return (result);
+}
+
 void	signal_handler(int sign_int)
 {
+	//printf("\nhere");
+	int	j;
 	int	c;
-	// printf("%3d : was recieved\n", sign_int);
-	c = 0;
+
 	if (sign_int == 30)
 	{
 		got[i] = 0;
@@ -18,20 +31,47 @@ void	signal_handler(int sign_int)
 		got[i] = 1;
 		i++;
 	}
-	if (i == 7)
+	ft_putchar('\n');
+	ft_putnbr(i);
+
+	//printf("\ngot signal %d hex now is : %d%d%d%d%d%d%d%d",
+	//		sign_int, got[0], got[1], got[2], got[3], got[4], got[5], got[6], got[7]);
+	if (i == 8)
 	{
-		while (i >= 0)
+		//printf("\n%d%d%d%d%d%d%d%d",
+		//	got[0], got[1], got[2], got[3], got[4], got[5], got[6], got[7]);
+		ft_putchar('\n');
+		ft_putnbr(got[0]);
+		ft_putnbr(got[1]);
+		ft_putnbr(got[2]);
+		ft_putnbr(got[3]);
+		ft_putnbr(got[4]);
+		ft_putnbr(got[5]);
+		ft_putnbr(got[6]);
+		ft_putnbr(got[7]);
+
+		j = 7;
+		i = 0;
+
+		while (i < 8)
 		{
-			c *= got[i] * i;
-			i--;
+			printf("\n%d += %d * 2 * %d", c, got[i], j);
+			ft_putnbr(c);ft_putchar(' ');ft_putnbr(got[i]);ft_putchar(' ');ft_putnbr(j);
+			c = c + got[i] * power(2, j);
+			i++;
+			j--;
 		}
-		write(1, &c, 1);
+		ft_putchar('\n');
+		ft_putchar(c);
+		ft_putchar('\n');
+		ft_putnbr(c);
 		i = 0;
 	}
 }
 
 int	main(void)
 {
+	i = 0;
 	got[0] = 0;
 	got[1] = 0;
 	got[2] = 0;
@@ -40,12 +80,16 @@ int	main(void)
 	got[5] = 0;
 	got[6] = 0;
 	got[7] = 0;
+
 	struct sigaction my_action;
 
 	my_action.sa_handler = signal_handler;
 
+	// printf("%d %d %d %d %d %d %d %d\n",
+	// 		got[0], got[1], got[2], got[3], got[4], got[5], got[6], got[7]);
+
 	ft_putnbr(getpid());
-	ft_putchar('\n');
+	// ft_putchar('\n');
 	while (1)
 	{
 		sigaction(SIGUSR1, &my_action, NULL);
